06756d0fc2225b4edf40cda751bb4a4035862e74
Fleming Kretschmer <flemingkretschmer@yahoo.de>
2022-04-27 12:03:28 +0200

    mpnranker2 also on GPU

--- mpnranker2.py
+++ mpnranker2.py
@@ -280,12 +280,15 @@ def train(ranker: MPNranker, bg: Union[BatchGenerator, DataLoader], epochs=2,
             break
         loop = tqdm(bg)
         for x, y, weights, is_confl in loop:
+            # move tensors/arrays to correct device
             if (ranker.extra_features_dim > 0):
                 x[0][1] = torch.as_tensor(x[0][1]).float().to(ranker.encoder.device)
                 x[1][1] = torch.as_tensor(x[1][1]).float().to(ranker.encoder.device)
             if (ranker.sys_features_dim > 0):
                 x[0][2] = torch.as_tensor(x[0][2]).float().to(ranker.encoder.device)
                 x[1][2] = torch.as_tensor(x[1][2]).float().to(ranker.encoder.device)
+            weights = torch.as_tensor(weights).to(ranker.encoder.device)
+            is_confl = torch.as_tensor(is_confl).to(ranker.encoder.device)
             ranker.zero_grad()
             loss = ranker.loss_step(x, y, weights, loss_fun)
             loss_sum += loss[0].item()
