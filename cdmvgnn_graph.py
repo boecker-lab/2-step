a32f11e7a764a3eb87ae7c94ccb4932e5f9637c7
Fleming Kretschmer <fleming.kretschmer@uni-jena.de>
2024-12-12 12:16:40 +0100

    delete unneeded

--- cdmvgnn_graph.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('../CD-MVGNN')
-from dglt.data.featurization.mol2graph import mol2graph
-
-def cdmvgnn_graph(smiles):
-    return mol2graph([smiles], {}, None)
--- system_lin.R
+++
@@ -1,3 +0,0 @@
-dss = read.csv('/home/fleming/Documents/Projects/rtdata_exploration/data/dataset_info_all.tsv', sep='\t')
-shared = read.csv('/home/fleming/Documents/Projects/rtdata_exploration/data/datasets_shared_orders.tsv', sep='\t')
-
